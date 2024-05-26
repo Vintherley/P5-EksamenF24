f06b26fcaa9556fc8378da560896abfc8c0e097b
Lotte89 <lotte.e.hansen@gmail.com>
2024-05-26 16:40:26 +0200

    Added step guide

---
+++ tringuide.js
@@ -0,0 +1,38 @@
+let steps = [
+    {
+        "title": "Trin 1",
+        "path": "./img/step-guide/ball-1.png"
+    },
+    {
+        "title": "Trin 2",
+        "path": "./img/step-guide/ball-2.png"
+    },
+    {
+        "title": "Trin 3",
+        "path": "./img/step-guide/ball-3.png"
+    }
+]
+
+let stepsContainer = document.querySelector('.steps-container');
+
+steps.forEach((step, index) => {
+    let headline = document.createElement('h2');
+    let image = document.createElement('img');
+    
+    
+    if (step.title) { 
+        headline.innerText = step.title;
+        image.alt = step.title;
+    } else {
+        headline.innerText = "Tringuide";
+        image.alt = "Tringuide";
+    }
+    
+    headline.classList.add('step-headline');
+
+    image.src = step.path;
+    image.classList.add('step-image');
+
+    stepsContainer.appendChild(headline);
+    stepsContainer.appendChild(image);
+});
